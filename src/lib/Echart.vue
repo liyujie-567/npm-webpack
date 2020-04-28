6d31270e5b945e0328b2dd9f762fbcb9215e1fa7
liyujie-567 <79983104@qq.com>
2020-04-28 16:17:41 +0800

    更新echart

--- src/lib/Echart.vue
+++ src/lib/Echart.vue
@@ -17,7 +17,7 @@ export default {
     eStyle: {
       type: Object,
       default() {
-          return {}
+        return {}
       }
     },
     // 图表属性
@@ -30,29 +30,42 @@ export default {
   },
   data () {
     return {
-      charts: ''
+      charts: {}
+    }
+  },
+  watch: {
+    option: {
+      handler(n, o) {
+        this.$nextTick(() => {
+          this.drawLine(this.id, n);
+        })
+      },
+      deep: true
     }
   },
   mounted () {
     this.$nextTick(() => {
-        this.drawLine(this.id)
+      this.drawLine(this.id, this.option)
     })
     window.addEventListener('resize', () => {
-        this.charts.resize()
+      this.charts.resize()
     })
   },
   methods: {
-    drawLine(id) {
+    drawLine(id, option) {
+      if (!id){
+        return false;
+      }
       this.charts = this.$echarts.init(document.getElementById(id));
       // 打开loading
       this.charts.showLoading({
-          text : '正在加载数据'
+        text : '正在加载数据'
       });
       // 组装图表
-      this.charts.setOption(this.option)
+      this.charts.setOption(option)
       // 关闭loading
-      if (this.option.series.length > 0) {
-          this.charts.hideLoading()
+      if (option.series.length > 0) {
+        this.charts.hideLoading()
       }
     }
   },
